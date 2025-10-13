951ede6597d1326e1020f63f193dc2f9fa5e7ffe
masaru_mottora <masaru.iza@mottora-25.com>
2025-10-14 03:54:38 +0900

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,48 @@
+Dim coloringEnabled As Boolean
+
+Private Sub Worksheet_Change(ByVal Target As Range)
+    If Not coloringEnabled Then Exit Sub
+    
+    Dim ws As Worksheet
+    Dim editRow As Long
+    Dim aoColumn As Long
+    Dim cellAddress As String
+    Dim lastRow As Long
+    Dim nextRow As Long
+    
+    Set ws = ActiveSheet
+    aoColumn = ws.Range("AO1").Column
+    editRow = Target.Row
+    
+    If Target.Column = aoColumn Then Exit Sub
+    If editRow = 1 Then Exit Sub
+    
+    cellAddress = Target.Address(False, False)
+    Application.EnableEvents = False
+    Target.Interior.Color = RGB(255, 255, 200)
+    
+    lastRow = ws.Cells(ws.Rows.Count, aoColumn).End(xlUp).Row
+    
+    If ws.Cells(1, aoColumn).Value = "" Then
+        nextRow = 1
+    Else
+        nextRow = lastRow + 1
+    End If
+    
+    ws.Cells(nextRow, aoColumn).Value = cellAddress
+    Application.EnableEvents = True
+End Sub
+
+Sub ToggleColoring()
+    coloringEnabled = Not coloringEnabled
+    
+    If coloringEnabled Then
+        MsgBox "確定しました（色付け機能ON）"
+    Else
+        MsgBox "確定を解除しました（色付け機能OFF）"
+    End If
+    
+    Dim targetSheet As Worksheet
+    Set targetSheet = ThisWorkbook.Sheets(1)
+    targetSheet.Range("AP1").Value = coloringEnabled
+End Sub
